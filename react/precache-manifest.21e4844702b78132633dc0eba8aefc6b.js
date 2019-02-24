493e68ca9c7dc86de43330d1e2ee56ef387b158f
giuliohome <giuliohome@gmail.com>
2019-02-24 21:20:04 +0100

    index renamed at home level

---
+++ react/precache-manifest.21e4844702b78132633dc0eba8aefc6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2ceef7fcbd35532b1b5",
+    "url": "/react/static/js/runtime~main.b2ceef7f.js"
+  },
+  {
+    "revision": "1dd3577d4c7e8766d048",
+    "url": "/react/static/js/main.1dd3577d.chunk.js"
+  },
+  {
+    "revision": "c8e6c7d1d1934b88aea7",
+    "url": "/react/static/js/2.c8e6c7d1.chunk.js"
+  },
+  {
+    "revision": "1dd3577d4c7e8766d048",
+    "url": "/react/static/css/main.6be18b54.chunk.css"
+  },
+  {
+    "revision": "9f9b472f4078b8b0c6cd86982923bd0d",
+    "url": "/react/index.html"
+  }
+];
